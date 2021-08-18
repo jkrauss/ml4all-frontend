0f966f89a5fb36ecc0434d03a835277e8a9fdae1
Titus Josse Breede <Titus@jumbobeans.de>
2021-08-19 00:33:23 +0200

    cleaned tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,21 @@
-const production = !process.env.ROLLUP_WATCH;
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("postcss-import"),
-    ...(production
-      ? [require("autoprefixer"), require("cssnano")({ preset: "default" })]
-      : []),
-  ],
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const config = {
+	plugins: [
+		//Some plugins, like postcss-nested, need to run before Tailwind,
+		tailwindcss(),
+		//But others, like autoprefixer, need to run after,
+		autoprefixer(),
+		!dev &&
+			cssnano({
+				preset: "default",
+			}),
+	],
 };
+
+module.exports = config;
